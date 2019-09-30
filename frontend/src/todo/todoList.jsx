287b1e41a6dea493f95486eb5397a6bee804343e
Nelson Sozinho <nelsonsozinho@gmail.com>
2019-09-30 08:52:08 -0400

    redux devtools extension

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import IconButton from '../template/IconButton'
+import {connect} from 'react-redux'
 
-export default props  => {
+const TodoList = props  => {
 
   const renderRows = () => {
     const list = props.list || []
@@ -37,3 +38,6 @@ export default props  => {
     </table>
   )
 }
+
+const mapStateToProps = state => ({list: state.todo.list})
+export default connect(mapStateToProps)(TodoList)
